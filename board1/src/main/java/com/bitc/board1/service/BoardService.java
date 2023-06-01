121a9fee0c45e381b81d5e8122297728cdd36766
ezjing <126551734+ezjing@users.noreply.github.com>
2023-06-01 19:45:28 +0900

    spring_0601(기능 소개, 게시판 만들기)

--- board1/src/main/java/com/bitc/board1/service/BoardService.java
+++ board1/src/main/java/com/bitc/board1/service/BoardService.java
@@ -4,6 +4,6 @@ import com.bitc.board1.dto.BoardDto;
 
 import java.util.List;
 
-public interface BoardService {
+public interface BoardService { 
     public List<BoardDto> selectBoardList() throws Exception;   // interface는 기본적으로 public이 붙음 안써줘도 됨
 }
