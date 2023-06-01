27bf703c8a0296f78bbb88916aeb8f3f78da8809
ezjing <126551734+ezjing@users.noreply.github.com>
2023-06-01 19:46:22 +0900

    spring_0601(기능 소개, 게시판 만들기)

--- board1/src/main/java/com/bitc/board1/configuration/DatabaseConfiguration.java
+++ board1/src/main/java/com/bitc/board1/configuration/DatabaseConfiguration.java
@@ -3,7 +3,7 @@ package com.bitc.board1.configuration;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionFactoryBean; 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
